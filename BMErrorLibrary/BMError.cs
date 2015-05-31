624c4b9b97448a4df5bdaf4e1713316286c4583e
karin <kalin000624@gmail.com>
2015-05-31 15:19:17 +0900

    BMErrorLibraryのErrorMessageOutputメソッドにbool値を追加 falseでMessageBoxを表示しない

--- BMErrorLibrary/BMError.cs
+++ BMErrorLibrary/BMError.cs
@@ -23,61 +23,11 @@ namespace BMErrorLibrary
         private static string errorFileName = errorDirectory + "\\Log_" + DateTime.Now.Year + "_" + DateTime.Now.Month + "_" + DateTime.Now.Day + ".txt";
 
         /// <summary>
-        /// 引数で受け取ったエラーメッセージをログファイルに出力し、メッセージボックスを表示する
-        /// </summary>
-        /// <param name="errorMessage"></param>
-        public static void ErrorMessageOutput(string errorMessage)
-        {
-            //エラーログ用のディレクトリが存在していなかったら作る
-            if (!System.IO.Directory.Exists(errorDirectory))
-            {
-                System.IO.DirectoryInfo di = System.IO.Directory.CreateDirectory(errorDirectory);
-            }
-
-            //エラーログ用のファイルが存在しなかったら作る
-            if (!System.IO.File.Exists(errorFileName))
-            {
-                // hStream が破棄されることを保証するために using を使用する
-                // 指定したパスのファイルを作成する
-                using (System.IO.FileStream hStream = System.IO.File.Create(errorFileName))
-                {
-                    // 作成時に返される FileStream を利用して閉じる
-                    if (hStream != null)
-                    {
-                        hStream.Close();
-                    }
-                }
-            }
-            
-            Encoding sjisEnc = Encoding.GetEncoding("Shift_JIS");
-            StreamWriter writer = new StreamWriter(errorFileName, true, sjisEnc);
-
-            //一つ前のスタックを取得
-            StackFrame callerFrame = new StackFrame(1);
-
-            //このメソッドの呼び出し元のメソッド名
-            string methodName = callerFrame.GetMethod().Name;
-
-            //このメソッドの呼び出し元のクラス名
-            string className = callerFrame.GetMethod().ReflectedType.FullName;
-
-            //出力用のテキストを定義
-            //現在の日付、呼び出し元情報、エラーメッセージ(引数)をくっつけてエラー文とする
-            string outputString = "[" + DateTime.Now + "]" + "Class = " + className + " ," 
-                + "Method = " + methodName + " " + errorMessage + "\n";
-
-            MessageBox.Show(errorMessage);
-
-            //ファイルに書き込み
-            writer.WriteLine(outputString);
-            writer.Close();
-        }
-
-        /// <summary>
         /// 引数で受け取ったエラーメッセージをログファイルに出力する
         /// </summary>
-        /// <param name="errorMessage"></param>
-        public static void ErrorMessageOutputNothingMessage(string errorMessage)
+        /// <param name="errorMessage">エラーメッセージ</param>
+        /// <param name="showMessageBox">MessageBoxを出すかどうか</param>
+        public static void ErrorMessageOutput(string errorMessage, bool showMessageBox)
         {
             //エラーログ用のディレクトリが存在していなかったら作る
             if (!System.IO.Directory.Exists(errorDirectory))
@@ -116,6 +66,9 @@ namespace BMErrorLibrary
             //現在の日付、呼び出し元情報、エラーメッセージ(引数)をくっつけてエラー文とする
             string outputString = "[" + DateTime.Now + "]" + "Class = " + className + " ,"
                 + "Method = " + methodName + " " + errorMessage + "\n";
+
+            if (showMessageBox)
+                MessageBox.Show(errorMessage);
 
             //ファイルに書き込み
             writer.WriteLine(outputString);

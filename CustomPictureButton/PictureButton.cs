ff889f24c935607b6723ef6c3e42b561a04fb31a
karin_0624 <kalin000624@gmail.com>
2015-12-15 14:52:10 +0900

    picturebuttonのついか

---
+++ CustomPictureButton/PictureButton.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomPictureButton
+{
+    public partial class PictureButton : UserControl
+    {
+        public Bitmap OffPicture { get; set; }
+        public Bitmap OnPicture { get; set; }
+
+        public PictureButton()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (OnPicture != null)
+            {
+                this.pictureBox1.Image = OffPicture;
+            }
+            else
+            {
+                this.pictureBox1.Image = null;
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (this.OnPicture != null)
+            {
+                this.pictureBox1.Image = OnPicture;
+            }
+            else
+            {
+                this.pictureBox1.Image = null;
+            }
+        }
+
+    }
+}
